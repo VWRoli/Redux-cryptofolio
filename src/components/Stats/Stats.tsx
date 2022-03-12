bc7fcacb45986b3b9e7831418d99351fbba614cd
VWRoli <fustroli@gmail.com>
2022-03-12 14:02:35 +0100

    feat: issue #22 hide chart on mobile

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,7 +1,13 @@
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
-import { FaChartLine, FaChartPie } from 'react-icons/fa';
+import {
+  FaChartLine,
+  FaChartPie,
+  FaChevronDown,
+  FaChevronUp,
+} from 'react-icons/fa';
 import { State } from '../../reducers';
+import useWindowWidth from '../../hooks/useWindowWidth';
 //Components
 import LineChartButtons from './LineChartButtons';
 import Error from '../common/Error/Error';
@@ -10,8 +16,12 @@ import PieChart from './PieChart';
 import Title from '../common/Title/Title';
 import Tab from '../common/Tab/Tab';
 import StatsHeader from './StatsHeader';
+import IconButton from '../common/IconButton/IconButton';
 
 const Stats: React.FC = (): JSX.Element => {
+  const width = useWindowWidth();
+  const isMobileScreen = width < 1050;
+  const [isOpen, setIsOpen] = useState(isMobileScreen ? false : true);
   const { isError } = useSelector((state: State) => state.asset);
   const [isLineChart, setIsLineChart] = useState(true);
 
@@ -25,23 +35,35 @@ const Stats: React.FC = (): JSX.Element => {
   return (
     <section id="stats">
       <Title h2 title="Overview" />
-
       <StatsHeader />
-      <div className="chart-tab-container">
-        <Tab
-          label="Line Chart"
-          icon={<FaChartLine />}
-          clickHandler={() => setIsLineChart(true)}
-        />
-        <Tab
-          label="Pie Chart"
-          icon={<FaChartPie />}
-          clickHandler={() => setIsLineChart(false)}
-        />
-      </div>
+      {isMobileScreen && (
+        <div className="show-stats-button">
+          <IconButton
+            icon={isOpen ? <FaChevronUp /> : <FaChevronDown />}
+            clickHandler={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      )}
 
-      {isLineChart ? <LineChart /> : <PieChart clicked={isLineChart} />}
-      {isLineChart ? <LineChartButtons /> : <></>}
+      {isOpen && (
+        <>
+          <div className="chart-tab-container">
+            <Tab
+              label="Line Chart"
+              icon={<FaChartLine />}
+              clickHandler={() => setIsLineChart(true)}
+            />
+            <Tab
+              label="Pie Chart"
+              icon={<FaChartPie />}
+              clickHandler={() => setIsLineChart(false)}
+            />
+          </div>
+
+          {isLineChart ? <LineChart /> : <PieChart clicked={isLineChart} />}
+          {isLineChart ? <LineChartButtons /> : <></>}
+        </>
+      )}
     </section>
   );
 };
